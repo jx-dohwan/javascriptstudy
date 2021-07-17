42cfb25857e2d4a4799970748baa548b86f31004
Do Hwan Go <jx7789@naver.com>
2021-07-17 13:48:09 +0900

    master

---
+++ HelloWorld.js
@@ -0,0 +1,10 @@
+let number = 1; // 정수
+let float = 3.14; //소수
+let string ="go"; //문자열
+let bool = false; //참거짓
+let boo2 = true; // 참거짓
+let tmp;
+
+console.log("되나");
+// var,let : 변수를 만들고 자유롭게 수정이 가능하다, 참고로 var는 거의 안쓴다. let만 쓴다고 생각해라
+//const : 변수를 만들고 수정이 불가능하다.
