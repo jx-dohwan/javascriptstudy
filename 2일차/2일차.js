68a046a0c94d7d69c8d4ee9082e375d24cbcdc3a
jx-dohwan <jx7789@naver.com>
2021-07-29 19:45:42 +0900

    master

---
+++ 2일차/2일차.js
@@ -0,0 +1,100 @@
+let A  = 10;
+let B = 20;
+
+let kim ="kim";
+let lee = "lee";
+
+lee ="kim";
+
+/*if(lee === kim){
+    console.log("같다.")
+}else if(lee == "lee"){
+    console.log("다르다")
+}else{
+    console.log("그런 이름 없다.")
+}*/
+
+//19 성인 남자면 출입 불가능
+//19 성인 여자면 출입 불가
+// let age = 19;
+// let gender = "남자";
+
+// let message =(age>19 && gender ==="남자")? "이용 불가." : "안녕";
+// console.log(message) 
+
+// let gender ="여자";
+// let age = 10;
+
+// if(gender!="남자"){
+//     console.log("여자입니다.")
+// }else if(gender==="남자" && age>=10){
+//     console.log("꼬맹이");
+// }else if(gender==="남자" && age<10){
+// console.log("애기");
+        
+// }else{
+//     console.log
+// }
+
+// var Name ="";
+
+// if(Name ==""){
+//     Name="kim"
+//     console.log(Name);
+// }
+
+// for(let i=0; i<=4; i++){
+    
+//     for(let j=1; j<i; j++){
+//         console.log('*');
+        
+//     }
+//     console.log("\n");
+// }
+
+// for (let i = 1; i <= 4; i++) {
+//     let star = "";
+//     for (let j = 0; j < i; j++) {
+//         console.log('*');
+//     }
+    
+//   }
+//   for (let i = 1; i <= 4; i++) {
+    
+//     for (let j = 0; j < i; j++) {
+//       console.log('*');
+//     }
+//     console.log('\n');
+//   }
+
+// for (let i = 0; i < 5; i++) {
+//     for (let j = 0; j <= i; j++) {
+//       console.log('*');
+//     }
+//     console.log('\n');
+//   }
+
+//   var star = '';
+// for (var i = 0; i < 5; i++) {
+//     star += '*';
+//     console.log(star);
+// }
+
+// let age = 30;
+
+// switch(age){
+//     case 0:
+        
+//         case 10:
+//             case 30:
+//                 case 40:
+//                     default:
+                        
+
+// }
+
+add(10,20);
+
+function add(a,b){
+    console.log(a+b);
+}
